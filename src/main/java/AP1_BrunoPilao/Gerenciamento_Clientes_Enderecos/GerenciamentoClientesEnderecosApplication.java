5f89eb2ccb45565aaac4237805b6319531c96479
brunopdrocha <brunopdrocha@gmail.com>
2024-09-20 22:00:52 -0300

    Primeiro Commit

---
+++ src/main/java/AP1_BrunoPilao/Gerenciamento_Clientes_Enderecos/GerenciamentoClientesEnderecosApplication.java
@@ -0,0 +1,13 @@
+package AP1_BrunoPilao.Gerenciamento_Clientes_Enderecos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoClientesEnderecosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoClientesEnderecosApplication.class, args);
+	}
+
+}
